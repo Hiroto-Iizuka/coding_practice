4b96e99202e4f165435a16594f8f218f7fd89be1
Hiroto-Iizuka <oroxith@gmail.com>
2025-12-26 22:28:34 +0900

    fix

--- 2_Add_Two_Numbers/step2.py
+++ 2_Add_Two_Numbers/step2.py
@@ -1,27 +1,25 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        last_fixed = dummy
-
-        n1 = l1
-        n2 = l2
+        answer = result_node = ListNode()
+        l1_node = l1
+        l2_node = l2
         carry = 0
-        while n1 or n2 or carry != 0:
-            val1 = n1.val if n1 else 0
-            val2 = n2.val if n2 else 0
+
+        while l1_node or l2_node or carry != 0:
+            val1 = l1_node.val if l1_node else 0
+            val2 = l2_node.val if l2_node else 0
             total = val1 + val2 + carry
             if total >= 10:
-                total -= 10
                 carry = 1
             else:
-                carry = 0
+                carry = 0            
+            result_node.next = ListNode(val=total % 10)
+            result_node = result_node.next
 
-            last_fixed.next = ListNode(val=total)
-            last_fixed = last_fixed.next
+            if l1_node:              
+                l1_node = l1_node.next
+            if l2_node:
+                l2_node = l2_node.next
 
-            if n1:
-                n1 = n1.next
-            if n2:
-                n2 = n2.next
-
-        return dummy.next
+        return answer.next
+    
